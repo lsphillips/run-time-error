18492fdc8c7e9b813258827d2c732decd2b57ebb
lsphillips <lsphillips@users.noreply.github.com>
2025-03-28 09:46:40 +0000

    Fixed the ESLint ignored files.

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import protectMeFromMyStupidity from 'eslint-config-protect-me-from-my-stupidity
 export default [
 	{
 		ignores : [
-			'/run-time-error.js',
+			'run-time-error.js',
 			'run-time-error.cjs',
 			'run-time-error.es5.js',
 			'run-time-error.es5.cjs'
